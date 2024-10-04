f55109ed63d4e0133f321663616f6393d52f01a5
MohanapriyaaV <136950646+MohanapriyaaV@users.noreply.github.com>
2024-10-04 09:54:37 +0530

    Time: 11 ms (45.29%) | Memory: 12.9 MB (56.17%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int mostWordsFound(vector<string>& sentences) {
-        int maxcount = 0;
+    int mostWordsFound(vector<string>& sentences) 
+    {
+        int count = 0 , maxLen = 0;
         for(int i=0;i<sentences.size();i++)
         {
-            int count = 1;
+           count = 0;
             for(int j=0;j<sentences[i].size();j++)
             {
-                if(sentences[i][j]==' ')
+                if(sentences[i][j] == ' ')
                 {
                     count++;
                 }
             }
-         maxcount = std::max(maxcount,count);
+           maxLen = max(count , maxLen);
         }
-        return maxcount;
+        return maxLen+1;
     }
 };
