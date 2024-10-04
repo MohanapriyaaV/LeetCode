52d3d152e3e99d19e9b8748e0798b248a536dece
MohanapriyaaV <136950646+MohanapriyaaV@users.noreply.github.com>
2024-10-04 09:36:19 +0530

    Time: 33 ms (99.80%) | Memory: 55.9 MB (97.20%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        const int n = skill.size() , n_2 = n/2;
+        long long sum = 0;
+        int freq[1001] = {0} , xMax=1 , xMin=1000;
+        for(int x:skill)
+        {
+            sum+=x;
+            freq[x]++;
+            xMax = max(x,xMax);
+            xMin = min(x,xMin);
+        }
+        if(sum%n_2!=0) return -1;
+        int teamSkill = sum/n_2;
+
+        long long chemi = 0;
+        int l , r;
+
+        for(l=xMin , r=xMax; l<r; l++ , r--)
+        {
+            long long fL = freq[l], fR=freq[r];
+            if(l+r!=teamSkill || fL!=fR) return -1;
+            chemi+=fL*l*r;
+        }
+        chemi+=(l==r && l*2==teamSkill)?freq[l]/2LL*l*l:0LL;
+        return chemi;
+        
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
